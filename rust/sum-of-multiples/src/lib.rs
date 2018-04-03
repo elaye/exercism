b4e81126390641860bc6d7863fea0936c784a1cf
Elie Génard <elie.genard@laposte.net>
2018-04-03 21:53:01 +0200

    Remove useless step

--- rust/sum-of-multiples/src/lib.rs
+++ rust/sum-of-multiples/src/lib.rs
@@ -9,7 +9,6 @@ fn quotient(n: u32, p: &u32) -> u32 {
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
     let mut multiples = factors
         .iter()
-        .filter(|factor| **factor < limit)
         .map(|factor: &u32| (1..quotient(limit, factor) + 1)
              .map(|i| i * factor).collect::<Vec<u32>>()
         )
